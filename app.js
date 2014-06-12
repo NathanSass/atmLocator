3af85b9438f332bc1e7f9d9e0bf3515f8fbba371
Nathan Sass <nsass711@gmail.com>
2014-06-12 16:05:34 -0700

    able to get back chase locations

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 	var app = angular.module('findAtm', []);
 	
 
-	app.controller('FindLocation', function($scope){
+	app.controller('FindLocation', function($scope, $http){
 		$scope.lat = "0";
     $scope.lng = "0";
 		$scope.model = { myMap: undefined };
@@ -16,12 +16,25 @@
 			}
     };
 
-		
-		$scope.showPosition = function(position){
+    $scope.useGeoLoc = function(position){
 			$scope.lat = position.coords.latitude;
       $scope.lng = position.coords.longitude;
+    	
+    	$scope.getNearbyAtm($scope.lat, $scope.lng);
+    	$scope.buildMap($scope.lat, $scope.lng);
+    };
 
-      var latlng = new google.maps.LatLng($scope.lat, $scope.lng);
+    $scope.getNearbyAtm = function(lat, lng){
+    	console.log('getting nearby atms');
+    	var chaseUrl = 'https://m.chase.com/PSRWeb/location/list.action?lat=' + lat + '&lng=' + lng;
+    	console.log(chaseUrl)
+    	$http({method: 'GET', url: chaseUrl}).success(function(data){console.log(data)}).error(function(data){console.log("We have an error")});
+    };
+
+		
+		$scope.buildMap = function(lat, lng){
+
+      var latlng = new google.maps.LatLng(lat, lng);
       var map = new google.maps.Map(document.getElementById("map_canvas"),
             $scope.mapOptions(latlng));
       //add markers here
@@ -35,7 +48,7 @@
 			if(navigator.geolocation){
 				console.log("in sucess for getLocation")
 				// console.log(navigator.geolocation.getCurrentPosition)
-				navigator.geolocation.getCurrentPosition($scope.showPosition, $scope.showError);
+				navigator.geolocation.getCurrentPosition($scope.useGeoLoc, $scope.showError);
 			}else{
 				$scope.error = "This browser does not support geolocation."
 				alert($scope.error)
