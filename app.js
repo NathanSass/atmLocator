818b2c8df89e67bb2cd6e056f3c47b56a99ac127
Nathan Sass <nsass711@gmail.com>
2014-06-12 16:22:55 -0700

    able to drop one marker TODO drop all markers, make them links and then figure out how to dynamically open a page

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 	app.controller('FindLocation', function($scope, $http){
 		$scope.lat = "0";
     $scope.lng = "0";
-		$scope.model = { myMap: undefined };
+		$scope.googleMap = undefined;
 		$scope.error = "";
 
 		$scope.mapOptions = function(currentLocation){
@@ -20,22 +20,38 @@
 			$scope.lat = position.coords.latitude;
       $scope.lng = position.coords.longitude;
     	
-    	$scope.getNearbyAtm($scope.lat, $scope.lng);
-    	$scope.buildMap($scope.lat, $scope.lng);
+    	$scope.getNearbyAtm();
+    	$scope.buildMap();
     };
 
-    $scope.getNearbyAtm = function(lat, lng){
-    	console.log('getting nearby atms');
-    	var chaseUrl = 'https://m.chase.com/PSRWeb/location/list.action?lat=' + lat + '&lng=' + lng;
-    	console.log(chaseUrl)
-    	$http({method: 'GET', url: chaseUrl}).success(function(data){console.log(data)}).error(function(data){console.log("We have an error")});
+    $scope.getNearbyAtm = function(){
+    	var chaseUrl = 'https://m.chase.com/PSRWeb/location/list.action?lat=' + $scope.lat + '&lng=' + $scope.lng;
+    	$http({method: 'GET', url: chaseUrl}).success($scope.markAtms).error(function(data){console.log("We have an error")});
     };
+
+    $scope.markAtms = function(atmData){
+    	console.log(atmData)
+    	var nearbyAtms = atmData.locations
+    	for (var i = 0; i < nearbyAtms.length; i++){}
+    	$scope.dropMarker($scope.lat, $scope.lng)
+    };
+
+    $scope.dropMarker = function(lat, lng){
+    	var atmLocation = new google.maps.LatLng(lat, lng)
+    	var marker = new google.maps.Marker({
+	      position: atmLocation,
+	      map: $scope.googleMap,
+	      title: 'Hello World!'
+		  });
+
+
+    }
 
 		
-		$scope.buildMap = function(lat, lng){
+		$scope.buildMap = function(){
 
-      var latlng = new google.maps.LatLng(lat, lng);
-      var map = new google.maps.Map(document.getElementById("map_canvas"),
+      var latlng = new google.maps.LatLng($scope.lat, $scope.lng);
+      $scope.googleMap = new google.maps.Map(document.getElementById("map_canvas"),
             $scope.mapOptions(latlng));
       //add markers here
 		};
@@ -46,13 +62,11 @@
 
 		$scope.getLocation = function(){
 			if(navigator.geolocation){
-				console.log("in sucess for getLocation")
-				// console.log(navigator.geolocation.getCurrentPosition)
 				navigator.geolocation.getCurrentPosition($scope.useGeoLoc, $scope.showError);
 			}else{
 				$scope.error = "This browser does not support geolocation."
 				alert($scope.error)
-				//promp the user for his/her city and convert to latlong
+				//prompt the user for his/her city and convert to latlong
 			}
 		};
 
