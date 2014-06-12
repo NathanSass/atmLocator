4a9b2fecd52ecf81586e594a30394bb970e03a34
Nathan Sass <nsass711@gmail.com>
2014-06-12 15:12:55 -0700

    setting up angular controller

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
 (function(){
 	var app = angular.module('findAtm', []);
-	app.controller('TestController', function(){
-		alert('test complete')
+	
+
+	app.controller('FindLocation', function($scope){
+		$scope.error = "";
+
+
+		$scope.showError = function(error){
+			alert("There is an error: " + error)
+		};
+		$scope.showPosition = function(position){
+			alert("in position")
+		};
+
+		$scope.getLocation = function(){
+			if(navigator.geolocation){
+				console.log("in sucess for getLocation")
+				// console.log(navigator.geolocation.getCurrentPosition)
+				navigator.geolocation.getCurrentPosition($scope.showPosition, $scope.showError);
+			}else{
+				$scope.error = "This browser does not support geolocation."
+				alert("in getLocation ERROR")
+				//promp the user for his/her city and convert to latlong
+			}
+		};
+
+		$scope.getLocation();
+
+
+
 	});
 
 })();
